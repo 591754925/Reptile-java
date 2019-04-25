c65f6f63f34395ab15199ec5c283ac3f27010aab
yhj <huanjin.yuan@foxmail.com>
2019-04-25 22:26:04 +0800

    创建项目

---
+++ ReptileJava/src/main/java/com/yhj/reptile/ReptileJavaApplication.java
@@ -0,0 +1,13 @@
+package com.yhj.reptile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReptileJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReptileJavaApplication.class, args);
+	}
+
+}
