a2a78aba7101335b22d6c4ac2c829936dfa7e287
yhj <huanjin.yuan@foxmail.com>
2019-04-26 11:12:25 +0800

    爬取网页第二弹
    测试失败
    使用了SeimiCrawler

---
+++ ReptileJava/src/main/java/com/yhj/reptile/web/util/boot.java
@@ -0,0 +1,10 @@
+package com.yhj.reptile.web.util;
+
+import cn.wanghaomiao.seimi.core.Seimi;
+
+public class boot {
+	public static void main(String[] args) {
+		Seimi s = new Seimi();
+		s.start("basic");
+	}
+}
